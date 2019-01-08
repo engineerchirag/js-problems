e78ecba7d948fd2ebcb5a603fcb4bb9000fbed43
Chirag goel <engineerchirag021@gmail.com>
2019-01-08 22:51:50 +0530

    Custom bind function

---
+++ src/js/fundamentals/myBind.js
@@ -0,0 +1,22 @@
+Function.prototype.myBind = function(context) {
+  var fn = this,
+    previousArgs = Array.prototype.slice(arguments, 1);
+  return function() {
+    var currentArgs = Array.prototype.slice(arguments);
+    return fn.call(context, ...previousArgs, ...currentArgs);
+  };
+};
+
+var a = {
+  b: 1,
+  c: 2,
+  sum: function() {
+    return this.b + this.c;
+  }
+};
+
+console.log(a.sum());
+var sum1 = a.sum;
+// console.log(sum1());
+var sum = a.sum.myBind(a);
+console.log(sum());
